a14c3ddf376ac44a2e6cf6c04afc96be03a24c43
Léopold Goudier <leopold.goudier@gmail.com>
2024-04-22 18:50:12 +0200

    add doc for connect and database files

--- db/connect.go
+++ db/connect.go
@@ -12,9 +12,10 @@ const (
 	ROUGE      = "\033[31;01;51m"
 )
 
-// Connect voit si la base de données est bien accessible
-
 func CheckConnection() {
+	/*
+		Function allowing you to see if the database is accessible and if the creation went well
+	*/
 	db, err := Connect(os.Getenv("USER"), os.Getenv("PASSWORD"))
 	if err != nil {
 		log.Fatalln(WHITEONRED, "Une erreur est survenue !", END, "\n\n", err, ROUGE, "\n\nEssayez de voir si la base de données est bien accessible en vérifiant que le conteneur docker est bien lancé avec 'docker compose up'.", END)
@@ -24,6 +25,7 @@ func CheckConnection() {
 	if err != nil {
 		log.Fatalln(WHITEONRED, "Une erreur est survenue !", END, "\n\n", err, ROUGE, "\n\nEssayez de voir si la base de données est bien accessible en vérifiant que le conteneur docker est bien lancé avec 'docker compose up'.", END)
 	}
+	// To create fake rooms
 	// for i := 0; i < 10; i++ {
 	// 	addRoom(db, "Room", rand.Intn(100))
 	// }
--- db/database.go
+++ db/database.go
@@ -3,11 +3,23 @@ package db
 import (
 	"database/sql"
 	"os"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func Connect(user string, password string) (*sql.DB, error) {
+	/*
+		Function to establish a connection to the database.
 
+		Parameters:
+			- user (string): Username for database authentication.
+			- password (string): Password for database authentication.
+
+		Returns:
+			(*sql.DB, error): Database connection object and any error encountered.
+	*/
+
+	// Retrieving environment variables for host, port and database name
 	host := os.Getenv("HOST")
 	port := os.Getenv("PORT")
 	dbname := os.Getenv("DBNAME")
@@ -19,6 +31,19 @@ func Connect(user string, password string) (*sql.DB, error) {
 }
 
 func createTable(db *sql.DB, table string, queryParams string) error {
+	/*
+		Function to create a table in the database.
+
+		Parameters:
+			- db (*sql.DB): Database connection object.
+			- table (string): Name of the table to create.
+			- queryParams (string): Query parameters for creating the table.
+
+		Returns:
+			error: Any error encountered during table creation.
+	*/
+
+	// Creation of the table if it does not already exist (avoid overwriting it)
 	_, err := db.Exec("CREATE TABLE IF NOT EXISTS " + table + " (id INT AUTO_INCREMENT PRIMARY KEY, " + queryParams + ")")
 	if err != nil {
 		return err
@@ -27,6 +52,15 @@ func createTable(db *sql.DB, table string, queryParams string) error {
 }
 
 func initDB(db *sql.DB) (*sql.DB, error) {
+	/*
+		Function to initialize the database with required tables.
+
+		Parameters:
+			- db (*sql.DB): Database connection object.
+
+		Returns:
+			(*sql.DB, error): Database connection object and any error encountered.
+	*/
 	err := createTable(db, "room", "name VARCHAR(255), capacity INT")
 	if err != nil {
 		return db, err
@@ -39,6 +73,17 @@ func initDB(db *sql.DB) (*sql.DB, error) {
 }
 
 func addRoom(db *sql.DB, name string, capacity int) error {
+	/*
+		Function to add a room to the database.
+
+		Parameters:
+			- db (*sql.DB): Database connection object.
+			- name (string): Name of the room.
+			- capacity (int): Capacity of the room.
+
+		Returns:
+			error: Any error encountered during room addition.
+	*/
 	_, err := db.Exec("INSERT INTO room (name, capacity) VALUES (?, ?)", name, capacity)
 	if err != nil {
 		return err
